1e2654cbbe316253fcf2173619e895a2fe46c34f
vanyslight <mayvany227@gmail.com>
2026-07-02 20:08:48 -0300

    validation of the NxN classroom activity

--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/../../config/bootstrap.php';
 
+use App\Models\ClinicalCondition;
 use Core\Database\Database;
 use Database\Populate\ExamTypesPopulate;
 use Database\Populate\UsersPopulate;
@@ -11,4 +12,11 @@ Database::migrate();
 UsersPopulate::populate();
 
 ExamTypesPopulate::populate();
+
+foreach (['Hipertensão', 'Diabetes', 'Asma'] as $conditionName) {
+    if (!ClinicalCondition::exists(['name' => $conditionName])) {
+        $condition = new ClinicalCondition(['name' => $conditionName]);
+        $condition->save();
+    }
+}
 
